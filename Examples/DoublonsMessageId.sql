15b9709224da5ab691163f17a05dedebe02f46a6
PLAIS Lionel <lionel.plais@ilp-web.net>
2026-02-12 22:28:33 +0100

    SQL query correction.

--- Examples/DoublonsMessageId.sql
+++ Examples/DoublonsMessageId.sql
@@ -1,5 +1,6 @@
 SELECT emails.message_id, COUNT(emails.message_id)
 FROM emails
 GROUP BY message_id
-ORDER BY COUNT(message_id)
+HAVING COUNT(message_id) > 1
+ORDER BY COUNT(message_id) DESC
 LIMIT 10;
